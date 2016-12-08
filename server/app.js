d860e436d412b3fc0e66c779f26ef145518da3eb
Alexandr <baluhins@gmail.com>
2016-12-08 13:50:12 +0200

    Fix

--- server/app.js
+++ server/app.js
@@ -94,7 +94,7 @@ app.post('/login', (req, res) => {
 
 let httpsServer = https.createServer(options, app);
 
-httpsServer.listen(8443, () => {
+api.listen(8443, () => {
     database.test().then(
         res => {
             console.log('Connection with database established!');
