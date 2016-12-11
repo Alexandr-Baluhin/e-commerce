558da42c9a2aceab9d5970247ace40c1df774cfa
Alexandr <baluhins@gmail.com>
2016-12-11 20:36:29 +0200

    Server headers fix

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(busboy());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Headers", "Content-Type,type,id");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,OPTIONS");
     next();
 });
