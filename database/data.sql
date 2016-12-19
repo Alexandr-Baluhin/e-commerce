b04eaac35af3a2626f5d32b787a2b308fee6cadb
Alexandr <baluhins@gmail.com>
2016-12-19 23:41:56 +0200

    New data to db

--- database/data.sql
+++ database/data.sql
@@ -15,7 +15,11 @@ INSERT INTO `Locations` (name, address, city_loc, email, phone, zip) VALUES
 --
 
 INSERT INTO `Employees` (email, password, salt, location_id) VALUES 
-('jn.riekp@gmail.com', '98475bf2acf4afd89fb58429e10d1f1ae7ad783e7589e0a362384f5e203eaad5', '631215350ba97b8fbd396d4c202742ded21b5a777c808271bf8bd65cbfbe18d9', 1);
+('rigas.pasvaldiba@gmail.com', '1d3c466a81b9661010b6767b3d8f7dad2bb9d2612322e559748dc366e89d7b85', 'd93bf207fcb1086455b852ab77e0f5ad5c8a63c2b74df0909985ad1b5e2c184a', 1),
+('jurmala.pasvaldiba@gmail.com', '648f57bd0e380e253c9c4720310510e195af246d460f9f4652ecf6d1c965cd50', 'f686d6fc61bac4320908b434ddf5b30b6f07fc7dcca60d6a22e8334633eec8b8', 2),
+('bauskas.pasvaldiba@gmail.com', 'a89879879e3054cf9c6f6e5e41f7bc8b59a8de182f2929b0301eee7fb0f7b84e', '0044a4eb660fd3e7421a92a50716d78bc94b97b292633111c022c76ac12eec6e', 3),
+('smiltenes.pasvaldiba@gmail.com', 'af239e5d082ba985dac2b258d2808ae707a384d6e2933b4e8f072a36f388bbc5', '40762a22ab66ad8aa40d299d5b2b803ce866eb3139c6efa686437bc9be1d14bb', 4),
+('liepajas.pasvaldiba@gmail.com', 'c2573df03e658d776bdf314e27e261e11cbf9e684fd874ec419e66fdef5d0da1', '984b65b1b7742b4a6eb90623b42c7f0923c9eec2007b3aaa5aa46a9a709a41be', 5);
 --
 -- Dumping data for table `LegalPersons`
 --
