b69d9abda8ca4b0d90b46aeebce256d20ef5e19a
Alexandr <baluhins@gmail.com>
2017-03-22 09:17:49 +0200

    Https fix

--- front/app/configurations/config.ts
+++ front/app/configurations/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
-	"HOSTNAME": "http://localhost",
+	"HOSTNAME": "https://localhost",
 	"HOSTNAME_PORT": "80",
-	"API": "http://localhost",
+	"API": "https://localhost",
 	"API_PORT": "8443"
 };
