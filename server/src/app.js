ed7625923ebd38b64c29808312e5d74f43e4945a
Alexandr <baluhins@gmail.com>
2017-04-03 20:38:12 +0300

    Added SSL check for server

--- server/src/app.js
+++ server/src/app.js
@@ -8,9 +8,11 @@ const https = require('https');
 const fs = require('fs');
 const busboy = require('connect-busboy');
 
+const useSSL = true;
+
 let options = {
-    key: fs.readFileSync(__dirname + '/../ssl/key.pem', 'utf8'),
-    cert: fs.readFileSync(__dirname + '/../ssl/server.crt', 'utf8')
+  key: fs.readFileSync(__dirname + '/../ssl/key.pem', 'utf8'),
+  cert: fs.readFileSync(__dirname + '/../ssl/server.crt', 'utf8')
 };
 
 let database = new DAL();
@@ -18,28 +20,28 @@ let database = new DAL();
 app.use(bodyParser.json());
 app.use(busboy());
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type,type,id");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,OPTIONS");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,type,id");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,OPTIONS");
+  next();
 });
 
 app.get('/', (req, res) => res.send({response: 'Hello from server!'}));
 
 app.get('/locations', (req, res) => {
-    database.getLocations(null).then(
-        result => res.send(result),
-        err => res.send(err)
-    );
+  database.getLocations(null).then(
+    result => res.send(result),
+    err => res.send(err)
+  );
 });
 
 app.get('/request/list', (req, res) => {
-    let type = req.headers.type;
-    let id = req.headers.id;
-    database.getList(type, id).then(
-        result => res.send(result),
-        err => res.send(err)
-    );
+  let type = req.headers.type;
+  let id = req.headers.id;
+  database.getList(type, id).then(
+    result => res.send(result),
+    err => res.send(err)
+  );
 });
 
 app.get('/request/map', (req, res) => {
@@ -51,66 +53,81 @@ app.get('/request/map', (req, res) => {
 });
 
 app.get('/request/:id', (req, res) => {
-    let id = req.params.id;
-    database.getRequest(id).then(
-        result => res.send(result),
-        err => res.send(err)
-    )
+  let id = req.params.id;
+  database.getRequest(id).then(
+    result => res.send(result),
+    err => res.send(err)
+  )
 });
 
 app.post('/upload', (req, res) => {
-    let stream;
-    req.pipe(req.busboy);
-    req.busboy.on('file', (fieldname, file, filename) => {
-        stream = fs.createWriteStream(__dirname + '/files/' + filename);
-        file.pipe(stream);
-    });
-    req.busboy.on('finish', () => {
-      res.send('Done!');
-    });
+  let stream;
+  req.pipe(req.busboy);
+  req.busboy.on('file', (fieldname, file, filename) => {
+    stream = fs.createWriteStream(__dirname + '/files/' + filename);
+    file.pipe(stream);
+  });
+  req.busboy.on('finish', () => {
+    res.send('Done!');
+  });
 });
 
 app.post('/request', (req, res) => {
-    let request = req.body.request;
-    let email = req.body.email;
-    database.postRequest(request, email).then(
-        result => res.send({ success: "Jūsu pieprasījums tiek saglabāts!" }),
-        err => res.send({ error: "Jūsu pieprasījums netiek saglabāts!", message: err })
-    )
+  let request = req.body.request;
+  let email = req.body.email;
+  database.postRequest(request, email).then(
+    result => res.send({success: "Jūsu pieprasījums tiek saglabāts!"}),
+    err => res.send({error: "Jūsu pieprasījums netiek saglabāts!", message: err})
+  )
 });
 
 app.put('/request', (req, res) => {
-    let employee = req.body.employee;
-    let user = req.body.user;
-    let request = req.body.request;
-    database.putRequest(employee, user, request).then(
-        result => res.send({ success: "Jūsu lēmums tiek pieņemts!" }),
-        err => res.send({ error: "Jūsu lēmums netiek pieņemts!", message: err })
-    )
+  let employee = req.body.employee;
+  let user = req.body.user;
+  let request = req.body.request;
+  database.putRequest(employee, user, request).then(
+    result => res.send({success: "Jūsu lēmums tiek pieņemts!"}),
+    err => res.send({error: "Jūsu lēmums netiek pieņemts!", message: err})
+  )
 });
 
 app.post('/login', (req, res) => {
-    let type = req.body.type;
-    let email = req.body.email;
-    let password = req.body.password;
+  let type = req.body.type;
+  let email = req.body.email;
+  let password = req.body.password;
 
-    database.postLogin(type, email, password).then(
-        result => res.send(result),
-        err => res.send(err)
-    );
+  database.postLogin(type, email, password).then(
+    result => res.send(result),
+    err => res.send(err)
+  );
 });
 
 let httpsServer = https.createServer(options, app);
 
-app.listen(8443, () => {
+if (useSSL) {
+  httpsServer.listen(8443, () => {
     database.test().then(
-        res => {
-            console.log('Connection with database established!');
-            console.log('App listening on port 8443!');
-        },
-        err => {
-            console.log('There ir error with database connection! Error: ' + err.code);
-            console.log('App is closing');
-            process.exit();
-        });
-});
+      res => {
+        console.log('Connection with database established!');
+        console.log('App listening on port 8443!');
+      },
+      err => {
+        console.log('There ir error with database connection! Error: ' + err.code);
+        console.log('App is closing');
+        process.exit();
+      });
+  });
+} else {
+  app.listen(8443, () => {
+    database.test().then(
+      res => {
+        console.log('Connection with database established!');
+        console.log('App listening on port 8443!');
+      },
+      err => {
+        console.log('There ir error with database connection! Error: ' + err.code);
+        console.log('App is closing');
+        process.exit();
+      });
+  });
+}
